2bedd20460622946c37fe5519cb614bf0120d68f
Jim <hostinghoster42@gmail.com>
2026-04-03 12:36:13 +0200

    new exclude.conf, working scanner

--- packages/scanner/src/scanner/main.py
+++ packages/scanner/src/scanner/main.py
@@ -1,8 +1,40 @@
+import subprocess
+
 from common.settings import MASSCAN_RATE
 
 
+def scan_java() -> subprocess.Popen[str]:
+    return subprocess.Popen(
+        [
+            "stdbuf",
+            "-oL",  # important
+            "masscan",
+            "0.0.0.0/0",
+            "-p25565",
+            "--rate",
+            str(MASSCAN_RATE),
+            "-oL",
+            "-",
+            "--excludefile",
+            "./exclude.conf",
+        ],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+
 def main() -> None:
-    print(MASSCAN_RATE)
+    while True:
+        proc = scan_java()
+
+        while True:
+            line = proc.stdout.readline().strip()  # type: ignore
+
+            if not line:
+                break
+
+            if "open" in line:
+                print(line, flush=True)
 
 
 if __name__ == "__main__":
